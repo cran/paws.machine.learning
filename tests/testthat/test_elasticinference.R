35002f6a3ef4baa58de73a326b68143677c7e86d
Dyfan Jones <dyfan.r.jones@gmail.com>
2023-02-08 12:10:50 +0000

    version 0.2.0

---
+++ tests/testthat/test_elasticinference.R
@@ -0,0 +1,9 @@
+svc <- paws::elasticinference()
+
+test_that("describe_accelerator_types", {
+  expect_error(svc$describe_accelerator_types(), NA)
+})
+
+test_that("describe_accelerators", {
+  expect_error(svc$describe_accelerators(), NA)
+})
